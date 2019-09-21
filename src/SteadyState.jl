7083b86878dbdc8b68203495ff6f459755820218
Z-Denis <zakari.denis@gmail.com>
2019-09-21 23:54:10 +0200

    Remove useless methods, add new interface.

--- src/SteadyState.jl
+++ src/SteadyState.jl
@@ -2,11 +2,13 @@ module SteadyState
 
 using QuantumOptics
 using IterativeSolvers, LinearMaps, LinearAlgebra, SparseArrays
-export bicgstabl!
+
+const T_blas = Union{Float64,Float32,ComplexF64,ComplexF32}
+isblascompatible(M::AbstractMatrix{T}) where {T<:Number} = typeof(M)<:DenseMatrix && eltype(M)<:T_blas
+
+include("interface.jl")
+export iterative, iterative!
+
 include("generic_method.jl")
-export steadystate_iterative!
-include("bicgstab.jl")
-export steadystate_bicg, steadystate_bicg!
-#include("indexing.jl")
 
 end # module
--- src/interface.jl
+++ src/interface.jl
@@ -1,10 +1,53 @@
-const T_blas = Union{Float64,Float32,ComplexF64,ComplexF32}
+"""
+    iterative!(rho0, H, J, [method!], args...; [log=false], kwargs...) -> rho[, log]
 
-isblascompatible(M::AbstractMatrix{T}) where {T<:Number} = typeof(M)<:DenseMatrix && eltype(M)<:T_blas
+Compute the steady state density matrix of a Hamiltonian and a set of jump
+operators by solving `L rho = 0` via an iterative method provided as argument.
 
-function iterative!(rho0::AbstractMatrix{T1}, H::AbstractMatrix{T2}, J::Vector, method!::Union{Function,Missing}, args...; kwargs...) where {T1<:Number,T2<:Number}
+# Arguments
+* `rho0`: Initial density operator.
+* `H`: Arbitrary operator specifying the Hamiltonian.
+* `J`: Vector containing all jump operators which can be of any arbitrary operator type.
+* `method!::Function`: The iterative method to be used. Defaults to
+`IterativeSolvers.bicgstabl!` or `IterativeSolvers.idrs!` depending on arguments' types.
+* `args...`: Further arguments are passed on to the iterative solver.
+* `kwargs...`: Further keyword arguments are passed on to the iterative solver.
+
+See also: [`iterative`](@ref)
+"""
+function iterative!(rho0::AbstractOperator{B,B}, H::AbstractOperator{B,B}, J::Vector, method!::Union{Function,Missing}=missing, args...; kwargs...) where {B<:Basis}
+    @assert all(typeof(j) <: AbstractOperator{B,B} for j in J) "Jump operators have incompatible bases."
+    # TODO: relax this condition
+    @assert all(typeof(j)==typeof(first(J)) for j in J) "Jump operators must all be of the same type."
+    iterative!(rho0.data,H.data,map(x->x.data,J),method!,args...;kwargs...)
+end
+
+"""
+    iterative(H, J, [method!], args...; [log=false], kwargs...) -> rho[, log]
+
+Compute the steady state density matrix of a Hamiltonian and a set of jump
+operators by solving `L rho = 0` via an iterative method provided as argument.
+
+# Arguments
+* `H`: Arbitrary operator specifying the Hamiltonian.
+* `J`: Vector containing all jump operators which can be of any arbitrary operator type.
+* `method!::Function`: The iterative method to be used. Defaults to
+`IterativeSolvers.bicgstabl!` or `IterativeSolvers.idrs!` depending on arguments' types.
+* `args...`: Further arguments are passed on to the iterative solver.
+* `kwargs...`: Further keyword arguments are passed on to the iterative solver.
+
+See also: [`iterative`](@ref)
+"""
+function iterative(H::AbstractOperator{B,B}, J::Vector, method!::Union{Function,Missing}=missing, args...; kwargs...) where {B<:Basis}
+    @assert all(typeof(j) <: AbstractOperator{B,B} for j in J) "Jump operators have incompatible bases."
+    # TODO: relax this condition
+    @assert all(typeof(j)==typeof(first(J)) for j in J) "Jump operators must all be of the same type."
+    iterative(H.data,map(x->x.data,J),method!,args...;kwargs...)
+end
+
+function iterative!(rho0::AbstractMatrix{T1}, H::AbstractMatrix{T2}, J::Vector, method!::Union{Function,Missing}=missing, args...; kwargs...) where {T1<:Number,T2<:Number}
     if ismissing(method!)
-        if isblascompatible(rho0) && isblascompatible(H) && isblascompatible(eltype(J))
+        if isblascompatible(rho0) && isblascompatible(H) && all(isblascompatible.(J))
             return steadystate_iterative!(rho0,H,J,bicgstabl!,args...;kwargs...)
         else
             return steadystate_iterative!(rho0,H,J,idrs!,args...;kwargs...)
@@ -15,22 +58,8 @@ function iterative!(rho0::AbstractMatrix{T1}, H::AbstractMatrix{T2}, J::Vector, 
     end
 end
 
-function iterative(H::AbstractMatrix{T}, J::Vector, method!::Union{Function,Missing}, args...; kwargs...) where {T<:Number}
+function iterative(H::AbstractMatrix{T}, J::Vector, method!::Union{Function,Missing}=missing, args...; kwargs...) where {T<:Number}
     rho0 = similar(H)
     rho0[1,1] = one(T)
     return iterative!(rho0,H,J,method!,args...;kwargs...)
-end
-
-function iterative!(rho0::AbstractOperator{B,B}, H::AbstractOperator{B,B}, J::Vector, method!::Union{Function,Missing}, args...; kwargs...) where {B<:Basis}
-    @assert all(typeof(j) <: AbstractOperator{B,B} for j in J) "Jump operators have incompatible bases."
-    # TODO: relax this condition
-    @assert all(typeof(j)==first(J) for j in J) "Jump operators must all be of the same type."
-    iterative!(rho0.data,H.data,map(x->x.data,J),method!,args...;kwargs...)
-end
-
-function iterative(H::AbstractOperator{B,B}, J::Vector, method!::Union{Function,Missing}, args...; kwargs...) where {B<:Basis}
-    @assert all(typeof(j) <: AbstractOperator{B,B} for j in J) "Jump operators have incompatible bases."
-    # TODO: relax this condition
-    @assert all(typeof(j)==first(J) for j in J) "Jump operators must all be of the same type."
-    iterative(H.data,map(x->x.data,J),method!,args...;kwargs...)
 end
