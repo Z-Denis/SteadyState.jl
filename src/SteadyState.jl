5b645ddadfd385e9403076972ab53d0f0544b9d0
Z-Denis <zakari.denis@gmail.com>
2019-09-22 12:02:33 +0200

    Export nothing.

--- src/SteadyState.jl
+++ src/SteadyState.jl
@@ -7,8 +7,6 @@ const T_blas = Union{Float64,Float32,ComplexF64,ComplexF32}
 isblascompatible(M::AbstractMatrix{T}) where {T<:Number} = typeof(M)<:DenseMatrix && eltype(M)<:T_blas
 
 include("interface.jl")
-export iterative, iterative!
-
 include("generic_method.jl")
 
 end # module
